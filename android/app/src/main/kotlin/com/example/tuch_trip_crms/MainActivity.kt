b8504ca768b78bd3d0476d3cbe6beb019160e122
adilthengilan <adilthengilan123@gmail.com>
2024-06-29 10:45:09 +0530

    firest_commit

---
+++ android/app/src/main/kotlin/com/example/tuch_trip_crms/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tuch_trip_crms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/layouting.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:tuch_trip_crms/layoutbuilder.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ResponsiveLayout(
+          DesktopView: DesktopView(),
+          MobileViews: MobileView(),
+          TabView: TabView()),
+    );
+  }
+}
+
+class DesktopView extends StatefulWidget {
+  const DesktopView({super.key});
+
+  @override
+  State<DesktopView> createState() => _DesktopViewState();
+}
+
+class _DesktopViewState extends State<DesktopView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class TabView extends StatefulWidget {
+  const TabView({super.key});
+
+  @override
+  State<TabView> createState() => _TabViewState();
+}
+
+class _TabViewState extends State<TabView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class MobileView extends StatefulWidget {
+  const MobileView({super.key});
+
+  @override
+  State<MobileView> createState() => _MobileViewState();
+}
+
+class _MobileViewState extends State<MobileView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
